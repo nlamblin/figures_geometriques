0880caf46beff49e82c7169a419cb557c1696943
Nicolas Lamblin <nicolas.lamblin7@ent.univ-lorraine.fr>
2015-05-18 10:58:50 +0200

    Ajout Polygone.

---
+++ src/figures_geometriques/Polygone.java
@@ -0,0 +1,34 @@
+package figures_geometriques;
+
+import java.awt.Graphics;
+
+public abstract class Polygone extends FigureColoree {
+
+	public Polygone() {
+		
+	}
+
+	@Override
+	public int nbClics() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean estDedans(int x, int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void modifierPoints(Point[] ps) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void affiche(Graphics g) {
+		
+	}
+
+	
+}
