d7691142c7a863009a0eda9b7f5f266c6246732b
Quentin FERRY <quentinferry@hotmail.fr>
2015-05-18 10:59:34 +0200

    ..//..

--- src/figures_geometriques/Fenetre.java
+++ src/figures_geometriques/Fenetre.java
@@ -14,16 +14,16 @@ public class Fenetre extends JFrame{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel jp = new JPanel();
 		dessin = new DessinFigures();
-		jp.setSize(w,h);
-		jp.add(new PanneauChoix(dessin));
-		jp.add(dessin);
+		jp.setPreferredSize(new Dimension(w,h));
 		setContentPane(jp);
 		pack();
+		jp.add(new PanneauChoix(dessin));
+		jp.add(dessin);
 		setVisible(true);
 		requestFocusInWindow();
 	}
 	
 	public static void main(String [] args) {
-		
+		Fenetre f = new Fenetre("Figure Géométrique", 1000, 1000);
 	}
 }
