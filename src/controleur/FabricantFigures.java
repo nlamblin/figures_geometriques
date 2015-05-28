9e2375c628026fe1f914c9258f02c9a2826a8257
Nicolas Lamblin <nicolas.lamblin7@ent.univ-lorraine.fr>
2015-05-28 08:39:54 +0200

    Modif en recherche de l'erreur.

--- src/controleur/FabricantFigures.java
+++ src/controleur/FabricantFigures.java
@@ -24,7 +24,6 @@ public class FabricantFigures extends MouseAdapter{
 	public void mousePressed(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 		if(MouseEvent.BUTTON1 == arg0.getButton()) {
-			System.out.println("passe à l'interieur");
 			if (nb_points > point_cliques.size()){
 				point_cliques.add(new Point(arg0.getX(),arg0.getY()));
 				figure_encours_de_fabrication.modifierPoints(point_cliques);
