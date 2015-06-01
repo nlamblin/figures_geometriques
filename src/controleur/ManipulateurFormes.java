8a0a2e3740822da809ef06fc0e666a06d2af70ec
Nicolas Lamblin <nicolas.lamblin7@ent.univ-lorraine.fr>
2015-06-01 11:07:43 +0200

    Création ManipulateurFormes.

---
+++ src/controleur/ManipulateurFormes.java
@@ -0,0 +1,61 @@
+package controleur;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import vue.DessinFigures;
+
+public class ManipulateurFormes implements MouseListener, MouseMotionListener {
+
+	private int indice, lastX, lastY;
+	private boolean trans;
+	
+	private ManipulateurFormes() {
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
