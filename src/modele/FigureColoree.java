d72654f05aa2e668b759e0b08fc6c27e6205a5b0
Quentin FERRY <quentinferry@hotmail.fr>
2015-06-04 16:38:25 +0200

    1515

--- src/modele/FigureColoree.java
+++ src/modele/FigureColoree.java
@@ -8,8 +8,8 @@ import java.util.Observable;
 
 public abstract class FigureColoree extends Observable{
 
-	public static final int TAILLE_CARRE_SELECTION = 20;
-	public static final int PERIPHERIE_CARRE_SELECTION = 20;
+	public static final int TAILLE_CARRE_SELECTION = 15;
+	public static final int PERIPHERIE_CARRE_SELECTION = 15;
 	private boolean selected;
 	protected Color couleur;
 	protected ArrayList<Point> tab_mem;
